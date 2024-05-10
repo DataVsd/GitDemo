d3fa4524bf490774c90e1a1dab43d3151257d5ed
DataVsd <joshithamuni@gmail.com>
2024-05-10 18:09:21 +0530

    first commit from branch

--- src/main/java/RahulShettyTut/pageobjects/SamplePage.java
+++ src/main/java/RahulShettyTut/pageobjects/SamplePage.java
@@ -1,10 +1,16 @@
 package RahulShettyTut.pageobjects;
 
+import org.testng.annotations.Test;
+
 public class SamplePage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello grom gitx");
 	}
-
+@Test
+public void tes()
+{
+	System.out.println("hello branch developed");
+}
 }
